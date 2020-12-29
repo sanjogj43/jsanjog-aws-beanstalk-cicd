e4a1120e5a9b2abfb5cb72a05fe362bf15b7a758
Jadhav <jsanjog@3c22fbc0c219.ant.amazon.com>
2020-12-29 13:50:27 -0800

    AWS cicd-code pipeline-modified yml per aws doc-added getmapping

--- src/main/java/com/jsanjog/aws/awselasticbeanstalkexample/HelloController.java
+++ src/main/java/com/jsanjog/aws/awselasticbeanstalkexample/HelloController.java
@@ -1,11 +1,14 @@
 package com.jsanjog.aws.awselasticbeanstalkexample;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+
+    @GetMapping
     public String  hello()
     {
         return "Hello World!!!";
