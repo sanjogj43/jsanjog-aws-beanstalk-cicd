823437a679d4573796b022c22c6879634d8c2d1e
Jadhav <jsanjog@3c22fbc0c219.ant.amazon.com>
2020-12-29 13:56:30 -0800

    AWS cicd-code pipeline-modified yml per aws doc-added extends

--- src/main/java/com/jsanjog/aws/awselasticbeanstalkexample/AwsElasticBeanstalkExampleApplication.java
+++ src/main/java/com/jsanjog/aws/awselasticbeanstalkexample/AwsElasticBeanstalkExampleApplication.java
@@ -2,9 +2,10 @@ package com.jsanjog.aws.awselasticbeanstalkexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsElasticBeanstalkExampleApplication {
+public class AwsElasticBeanstalkExampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsElasticBeanstalkExampleApplication.class, args);
